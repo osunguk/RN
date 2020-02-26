dfefe42ce50f8d77ba14d7599b71e173701709ad
osunguk <osunguk@gmail.com>
2020-02-26 17:44:13 +0900

    팝업창 생성시 화면이 위로 올라가는 오류 (해결중)

--- third/src/popup/base.js
+++ third/src/popup/base.js
@@ -5,8 +5,8 @@ import { createStackNavigator } from 'react-navigation-stack'
 class base extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'  }}>
-        <View style={{  backgroundColor: 'red' }}><Text style={{ textAlign: 'left' }}>test</Text></View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ backgroundColor: 'red' }}><Text style={{ textAlign: 'left' }}>test</Text></View>
         <Text style={{ marginBottom: 20 }}>
           pop up text screen
         </Text>
@@ -22,82 +22,89 @@ class base extends React.Component {
 }
 
 class modal extends React.Component {
-  static navigationOptions = {
-    cardStyle: {
-      backgroundColor: 'transparent'
-    }
-  }
+  
 
   render() {
     return (
-      <TouchableOpacity
-        style={{flex:1, justifyContent:'center', alignItems:'center', flexDirection:'column',overflow:'hidden'}}
-        onPress={() => {
-          this.props.navigation.pop()
-        }}
-        activeOpacity={1}
-      >
-        <View style={{ backgroundColor: '#fefefe', backgroundColor: '#fefefe', paddingTop: 10, borderRadius: 10, overflow: 'hidden' }}>
-          <Text style={{ fontSize: 25, color: 'mediumseagreen', textAlign: 'center' }}>
-            제로고
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', overflow: 'hidden' }}
+          onPress={() => {
+            this.props.navigation.pop()
+          }}
+          activeOpacity={1}
+        >
+          <View style={{ backgroundColor: '#fefefe', backgroundColor: '#fefefe', paddingTop: 10, borderRadius: 10, overflow: 'hidden' }}>
+            <Text style={{ fontSize: 25, color: 'mediumseagreen', textAlign: 'center' }}>
+              제로고
             <Text style={{ fontSize: 25, color: 'mediumseagreen', fontWeight: 'bold', textAlign: 'center' }}>
-              {' '}iOS 앱 출시!
+                {' '}iOS 앱 출시!
             </Text>
-          </Text>
+            </Text>
 
-          <Text style={{ marginTop: 10, textAlign: 'center' }}>
-            이제 아이폰을 사용하는 친구와 {'\n'}
-            <Text>
-              함께 제로고 해요!
+            <Text style={{ marginTop: 10, textAlign: 'center' }}>
+              이제 아이폰을 사용하는 친구와 {'\n'}
+              <Text>
+                함께 제로고 해요!
             </Text>
-          </Text>
-          <Image
-            source={require('./test.png')}
-            style={{ width: 300, height: 150, resizeMode: 'contain' }}
-          />
-          <Text style={{ margin: 20, textAlign: 'center' }}>
-            친구와 함께{' '}
-            <Text style={{ alignSelf: 'center', fontWeight: 'bold', textAlign: 'center' }}>
-              2,000골드
+            </Text>
+            <Image
+              source={require('./test.png')}
+              style={{ width: 300, height: 150, resizeMode: 'contain' }}
+            />
+            <Text style={{ margin: 20, textAlign: 'center' }}>
+              친구와 함께{' '}
+              <Text style={{ alignSelf: 'center', fontWeight: 'bold', textAlign: 'center' }}>
+                2,000골드
             </Text>
            도 받으세요
           </Text>
-          <View style={{ flexDirection: 'row'}}>
-            <View style={{ flex: 1 }}>
-              <TouchableOpacity
-                activeOpacity={0.5}
-                onPress={() => {
-                  this.props.navigation.navigate('base')
-                }}
-                style={{alignItems:'center', backgroundColor:'gray', padding:15}}
-              >
-                <Text style={{color:'white', fontWeight:'bold', fontSize:15}}> 오늘 닫기 </Text>
-              </TouchableOpacity>
-            </View>
-            <View style={{ flex: 2 }}>
-              <TouchableOpacity
-                activeOpacity={0.5}
-                onPress={() => {
-                  this.props.navigation.navigate('testNavi')
-                }}
-                style={{alignItems:'center', backgroundColor:'black', padding:15}}
-              >
-                  <Text style={{color:'white', fontWeight:'bold', fontSize:15}}> 친구 초대 </Text>
-              </TouchableOpacity>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ flex: 1 }}>
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  onPress={() => {
+                    this.props.navigation.navigate('base')
+                  }}
+                  style={{ alignItems: 'center', backgroundColor: 'gray', padding: 15 }}
+                >
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15 }}> 오늘 닫기 </Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ flex: 2 }}>
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  onPress={() => {
+                    this.props.navigation.navigate('testNavi')
+                  }}
+                  style={{ alignItems: 'center', backgroundColor: 'black', padding: 15 }}
+                >
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15 }}> 친구 초대 </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
         </TouchableOpacity>
+      </View>
     )
+
   }
 }
 
 const RootStack = createStackNavigator({
   base: base,
-  modal: modal
+  modal: {
+    screen:modal,
+    navigationOptions:{
+      cardStyle: {
+        backgroundColor: 'transparent'
+      }
+    }
+  }
 }, {
   mode: 'modal',
   headerMode: 'none',
+  transparentCard: true,
   
 })
 
