88cd916df6ea4b2c5a4fbf4d727565547b7cf6a1
osunguk <osunguk@gmail.com>
2020-02-28 11:57:02 +0900

    실제 안드로이드 기기에서 돌리기 위한 prj

---
+++ fours/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'skyblue'}}>
+                <Text>Test real machine app</Text>
+                <Text>fours prj</Text>
+            </View>
+
+        )
+    }
+}
