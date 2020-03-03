3447470909e19983223c7ab2caa3494e98d6a95d
osunguk <osunguk@gmail.com>
2020-03-03 18:37:34 +0900

    pdf 패키지를 이용한 js 파일 추가

--- third/src/pdf/pdf.js
+++ third/src/pdf/pdf.js
@@ -1,106 +1,157 @@
 import React from 'react'
 import { View, Text, Dimensions, ActivityIndicator, Button } from 'react-native'
 import Pdf from 'react-native-pdf'
+import { TextInput } from 'react-native-gesture-handler'
 
 export default class PDFtest extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.getParam('title', 'temp title')
+
+    static navigationOptions = ({ navigation }) => {
+        if (navigation.state.params && !navigation.state.params.removeTitleBar) {
+            return {
+                title: navigation.getParam('title', 'temp title')
+            }
+        } else {
+            return {
+                headerShown: false
+            }
+        }
     }
-  }
 
-  state = {
-    error: false,
-    errorLog: '',
-    title: this.props.navigation.state.params.title,
-    source: {},
-    check: false
-  }
-
-  async componentDidMount() {
-    const isBase64 = await this.props.navigation.state.params.isBase64
-    if (isBase64) {
-      this.setState({
-        source: { uri: "data:application/pdf;base64,".concat(this.props.navigation.state.params.uri) }
-      }, () => {
-        this._show()
-      })
-
-    } else {
-      this.setState({
-        source: { uri: this.props.navigation.state.params.uri }
-      }, () => {
-        this._show()
-      })
+    state = {
+        error: false,
+        errorLog: '',
+        title: this.props.navigation.state.params.title,
+        source: {},
+        check: false,
+        numberOfPages: 0,
+        page: 1
     }
-  }
 
-  _show = () => {
-    this.setState({
-      check:true
-    })
-  }
+    onRemoveTitleBar = () => {
+        this.props.navigation.setParams({
+            removeTitleBar: true
+        })
+    }
 
-  render() {
-    if (this.state.check) {
-      if (this.state.error) {
-        return (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ textAlign: 'center' }}>
-              에러 발생! {'\n'} {this.state.errorLog}
-            </Text>
-            <Button
-              title='다시 시도하기'
-              onPress={() => {
-                this.props.navigation.navigate('OpenFile')
-              }}
-            />
-          </View>
-        )
-      } else {
-        return (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Pdf
-              ref={(pdf) => { this.pdf = pdf }}
-              source={this.state.source} // open 할 paf 파일 소스
-              // page={110} 초기 페이지 설정
-              onLoadProgress={() => {
-                // 로딩중에 돌릴 함수
-              }}
-              onLoadComplete={(numberOfPages, filePath) => {
-                // console.log(numberOfPages, filePath)
-                // 로딩 다 되면
-              }}
-              onPageChanged={(page, numberOfPages) => {
-                // console.log(page, numberOfPages)
-                // 페이지 넘어가면
-              }}
-              onError={(error) => {
-                this.setState({
-                  error: true,
-                  errorLog: error.toString()
-                })
-              }}
-              onPressLink={(uri) => {
+    componentDidMount() {
 
-              }}
-              activityIndicator={
+        const isBase64 = this.props.navigation.state.params.isBase64
+        if (isBase64) {
+            this.setState({
+                source: { uri: "data:application/pdf;base64,".concat(this.props.navigation.state.params.uri) }
+            }, () => {
+                this._show()
+            })
+
+        } else {
+            this.setState({
+                source: { uri: this.props.navigation.state.params.uri }
+            }, () => {
+                this._show()
+            })
+        }
+    }
+
+    _show = () => {
+        this.setState({
+            check: true
+        })
+    }
+
+    _retry = () => {
+        this.setState({
+            error: false
+        })
+    }
+
+    _paging = (page) => {
+        this.pdf.setpage(parseInt(page))
+    }
+
+    render() {
+        if (this.state.check) {
+            if (this.state.error) {
+                return (
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ textAlign: 'center' }}>
+                            에러 발생! {'\n'}{'\n'} {this.state.errorLog}
+                        </Text>
+                        <View style={{ flexDirection: 'row' }}>
+                            <View style={{ flex: 1, margin: 30 }}>
+                                <Button
+                                    title='재시도'
+                                    onPress={() => {
+                                        this._retry()
+                                    }}
+                                />
+                            </View>
+                            <View style={{ flex: 1, margin: 30 }}>
+                                <Button
+                                    title='돌아가기'
+                                    onPress={() => {
+                                        this.props.navigation.navigate('OpenFile')
+                                    }}
+                                />
+                            </View>
+                        </View>
+                    </View>
+                )
+            } else {
+                return (
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <Pdf
+                            ref={(pdf) => { this.pdf = pdf }}
+                            source={this.state.source} // open 할 paf 파일 소스
+                            // page={110} 초기 페이지 설정
+                            onLoadProgress={() => {
+                                // 로딩중에 돌릴 함수
+                            }}
+                            onLoadComplete={(numberOfPages, filePath) => {
+                                this.setState({
+                                    numberOfPages: numberOfPages
+                                })
+                            }}
+                            onPageChanged={(page, numberOfPages) => {
+                                // this._paging(page)
+                            }}
+                            onError={(error) => {
+                                this.setState({
+                                    error: true,
+                                    errorLog: error.toString(),
+                                    showTitleBar: false
+                                })
+                                this.onRemoveTitleBar()
+                            }}
+                            onPressLink={(uri) => {
+
+                            }}
+                            activityIndicator={
+                                <ActivityIndicator />
+                            }
+                            style={{ flex: 1, width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
+                        />
+                        <View style={{ backgroundColor: 'gray', height: Dimensions.get('window').height * 0.08, width: Dimensions.get('window').width, justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                            <View style={{ backgroundColor: 'white', height: Dimensions.get('window').height * 0.05 }}>
+                                <TextInput
+                                    keyboardType='numeric'
+                                    value={this.state.page}
+                                    // onChangeText={(number) => {
+                                    //     if (number) {
+                                    //         this._paging(number)
+                                    //     }
+                                    // }}
+                                />
+                            </View>
+                            <View style={{ marginLeft: 3 }}><Text>/ {this.state.numberOfPages}</Text></View>
+                        </View>
+                    </View>
+                )
+            }
+        } else {
+            return (
                 <ActivityIndicator />
-              }
-              style={{ flex: 1, width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
-            />
-            <Button
-              title='go first'
-              onPress={() => {
-                this.pdf.setPage(1)
-              }}
-            />
-          </View>
-        )
-      }
-    } else {
-      return null
+            )
+        }
     }
-  }
 
 }
